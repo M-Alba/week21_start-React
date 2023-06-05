890b2c56520378696bb419034351e91dccdb4c21
Albina Mukhamadeeva <albina_ips@list.ru>
2023-06-05 22:57:25 +0500

    added pie chart

---
+++ week21app/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  Chart
+} from "react-google-charts";
+
+export const data = [
+  ["Материк", "Площадь"],
+  ["Евразия", 53.89],
+  ["Африка", 30.25],
+  ["Северная Америка", 24.2],
+  ["Южная Америка", 17.82],
+  ["Антарктида", 13.66],
+  ["Австралия ", 7.69],
+];
+
+export const options = {
+  title: "Площадь материков",
+};
+
+function App() {
+  return ( <
+    div className = "App" >
+    <
+    Chart chartType = "PieChart"
+    data = {
+      data
+    }
+    options = {
+      options
+    }
+    width = {
+      "100%"
+    }
+    height = {
+      "600px"
+    }
+    /> < /
+    div >
+  );
+}
+
+export default App;
